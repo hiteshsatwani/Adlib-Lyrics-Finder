e83cdf009488658685a0a3bd8edcc6f5b4a944d0
Hitesh <hiteshsatwani74@gmail.com>
2020-10-26 19:17:39 +0400

    localhost added

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -25,7 +25,7 @@ class Navbar extends Component {
                         )
                     })}
                 </ul>
-                <a href="https://spotify-auth-adlib.herokuapp.com:8888"><Button>< i className="fab fa-spotify"></i> Login To Spotify </Button></a>
+                <a href="http://localhost:8888"><Button>< i className="fab fa-spotify"></i> Login To Spotify </Button></a>
             </nav>
         )
     }
