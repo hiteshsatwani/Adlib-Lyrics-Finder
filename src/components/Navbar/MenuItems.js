308a420e74251ccb34f01f6024aa1f62bebc7885
Hitesh <hiteshsatwani74@gmail.com>
2020-10-26 22:53:55 +0400

    phone test

--- src/components/Navbar/MenuItems.js
+++ src/components/Navbar/MenuItems.js
@@ -14,10 +14,6 @@ export const MenuItems = [
         url: '#',
         cName: 'nav-links'
     },
-    {
-        title: 'Log In',
-        url: '#',
-        cName: 'nav-links-mobile'
-    },
+
 
 ]
